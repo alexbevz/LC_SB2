5bb5b22fe903751ce90c38658ce623dbca2b2892
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-12 19:50:45 +0300

    Complete 7th lesson
    Added a style by style.css.
    Updated a property file.
    Updated a templates.
    Updated a containing java-application.

--- src/main/java/ru/bevz/LC_SB2/controller/MainController.java
+++ src/main/java/ru/bevz/LC_SB2/controller/MainController.java
@@ -1,15 +1,21 @@
 package ru.bevz.LC_SB2.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.bevz.LC_SB2.domain.Message;
 import ru.bevz.LC_SB2.domain.User;
 import ru.bevz.LC_SB2.repos.MessageRepo;
 
+import java.io.File;
+import java.io.IOException;
+import java.rmi.AccessException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
@@ -19,6 +25,9 @@ public class MainController {
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
     }
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -49,9 +58,24 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model) {
+            @RequestParam MultipartFile file) throws IOException {
 
         Message message = new Message(text, tag, user);
+
+        if (!file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                if (!uploadDir.mkdir()) {
+                    throw new AccessException("You are innocent! Some problem on the server!");
+                }
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+
+            message.setFilename(resultFileName);
+        }
 
         messageRepo.save(message);
 
