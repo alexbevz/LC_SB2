fd26c52bcf60d6f52d07f5f56d80b4262555a13d
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-13 20:38:59 +0300

    Update project structure
    Added a hiding file with properties.
    Updated a main file properties.
    Updated a class Application: add a new property source.

--- src/main/java/ru/bevz/LC_SB2/Application.java
+++ src/main/java/ru/bevz/LC_SB2/Application.java
@@ -2,8 +2,10 @@ package ru.bevz.LC_SB2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:/hiding.properties")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
