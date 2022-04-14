7f49cb5bde3b8f6838bad943d4508b49daa95799
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-14 10:43:57 +0300

    Add a system GlobalExceptionHandler
    Added a controller advice GlobalExceptionHandler with one method that handle any exception.
    Added a view error.
    Updated a config WebSecurityConfig.

--- src/main/java/ru/bevz/LC_SB2/config/WebSecurityConfig.java
+++ src/main/java/ru/bevz/LC_SB2/config/WebSecurityConfig.java
@@ -28,7 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             "/",
                             "/registration",
                             "/activate/*",
-                            "/static/**"
+                            "/static/**",
+                            "/error"
 
                     ).permitAll()
                     .anyRequest().authenticated()
---
+++ src/main/java/ru/bevz/LC_SB2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package ru.bevz.LC_SB2.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public String handleAnyException(
+            Exception exception,
+            Model model
+    ) {
+
+        model.addAttribute("infoError", exception.getMessage());
+
+        return "error";
+    }
+}
