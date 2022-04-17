2704b445051953e41e0a9ca4c17c214129a2bddd
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-18 01:50:19 +0300

    Complete 12th lesson

--- src/main/java/ru/bevz/LC_SB2/controller/RegistrationController.java
+++ src/main/java/ru/bevz/LC_SB2/controller/RegistrationController.java
@@ -1,23 +1,41 @@
 package ru.bevz.LC_SB2.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import ru.bevz.LC_SB2.domain.User;
+import ru.bevz.LC_SB2.domain.dto.CaptchaResponseDto;
 import ru.bevz.LC_SB2.service.UserService;
 
 import javax.validation.Valid;
+import java.util.Collections;
 
 @Controller
 public class RegistrationController {
 
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     private final UserService userService;
 
-    public RegistrationController(UserService userService) {
+    private final RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String recaptchaSecret;
+
+    @Value("${recaptcha.html}")
+    private String recaptchaHtml;
+
+    @ModelAttribute(name = "recaptchaKey")
+    private String getRecaptchaHtml() {
+        return recaptchaHtml;
+    }
+
+    public RegistrationController(UserService userService, RestTemplate restTemplate) {
         this.userService = userService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/registration")
@@ -27,25 +45,35 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(
+            @RequestParam("g-recaptcha-response") String captchaResponse,
             @Valid User user,
-            BindingResult bindingResult,
+            BindingResult result,
             Model model
     ) {
+        String url = String.format(CAPTCHA_URL, recaptchaSecret, captchaResponse);
+
+        CaptchaResponseDto response =
+                restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (response != null && !response.isSuccess()) {
+            //TODO: to need to implement by BindingResult
+            model.addAttribute( "captchaError", "fill captcha");
+        }
 
         if (user.getPassword() != null && !user.getConfirmPassword().equals(user.getPassword())) {
-            bindingResult.rejectValue("confirmPassword", "confirmPasswordError", "passwords are different");
+            result.rejectValue("confirmPassword", "confirmPasswordError", "passwords are different");
         }
 
         if (userService.findByEmail(user.getEmail()) != null) {
-            bindingResult.rejectValue("email", "emailError", "this email already uses");
+            result.rejectValue("email", "emailError", "this email already uses");
         }
 
         if (userService.findByUsername(user.getUsername()) != null) {
-            bindingResult.rejectValue("username", "usernameError", "user exists");
+            result.rejectValue("username", "usernameError", "user exists");
         }
 
-        if (bindingResult.hasErrors()) {
-            model.mergeAttributes(ControllerUtils.getErrors(bindingResult));
+        if (result.hasErrors() || (response != null && !response.isSuccess())) {
+            model.mergeAttributes(ControllerUtils.getErrors(result));
             return "registration";
         }
 
@@ -60,8 +88,10 @@ public class RegistrationController {
 
         if (isActivate) {
             model.addAttribute("message", "User successfully activated!");
+            model.addAttribute("messageType", "success");
         } else {
             model.addAttribute("message", "Activate code is not found!");
+            model.addAttribute("messageType", "danger");
         }
 
         return "login";
