fe4f6c5fc2e87801992a44e7b604415b9ee1ac86
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-28 23:28:42 +0300

    Update repository MessageRepo
    Updated all methods.

--- src/main/java/ru/bevz/LC_SB2/repos/MessageRepo.java
+++ src/main/java/ru/bevz/LC_SB2/repos/MessageRepo.java
@@ -2,18 +2,55 @@ package ru.bevz.LC_SB2.repos;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.bevz.LC_SB2.domain.Message;
 import ru.bevz.LC_SB2.domain.User;
+import ru.bevz.LC_SB2.domain.dto.MessageDto;
 
 @Repository
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    Page<Message> findAll(Pageable pageable);
+    @Query(
+            """
+            select new ru.bevz.LC_SB2.domain.dto.MessageDto(
+            m,
+            count(ml),
+            sum(case when ml = :user then 1 else 0 end) > 0
+            )
+            from Message m left join m.likes ml
+            group by m
+            """
+    )
+    Page<MessageDto> findAll(Pageable pageable, User user);
 
-    Page<Message> findByTag(String tag, Pageable pageable);
+    @Query(
+            """
+            select new ru.bevz.LC_SB2.domain.dto.MessageDto(
+            m,
+            count(ml),
+            sum(case when ml = :user then 1 else 0 end) > 0
+            )
+            from Message m left join m.likes ml
+            where m.tag = :tag
+            group by m
+            """
+    )
+    Page<MessageDto> findByTag(String tag, Pageable pageable, User user);
 
-    Page<Message> findByAuthor(User user, Pageable pageable);
+    @Query(
+            """
+            select new ru.bevz.LC_SB2.domain.dto.MessageDto(
+            m,
+            count(ml),
+            sum(case when ml = :user then 1 else 0 end) > 0
+            )
+            from Message m left join m.likes ml
+            where m.author = :author
+            group by m
+            """
+    )
+    Page<MessageDto> findByAuthor(Pageable pageable, User author, User user);
 
 }
