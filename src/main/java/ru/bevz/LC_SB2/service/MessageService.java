b3d1248aaad3b0e46a8facabb0c4fd5c3c963bbf
Bevz Aleksandr <as-bivz@yandex.ru>
2022-04-28 23:29:36 +0300

    Update service MessageService
    Add method that was moved from controller.

--- src/main/java/ru/bevz/LC_SB2/service/MessageService.java
+++ src/main/java/ru/bevz/LC_SB2/service/MessageService.java
@@ -1,11 +1,16 @@
 package ru.bevz.LC_SB2.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.bevz.LC_SB2.domain.Message;
+import ru.bevz.LC_SB2.domain.User;
+import ru.bevz.LC_SB2.domain.dto.MessageDto;
 import ru.bevz.LC_SB2.repos.MessageRepo;
 
+import javax.persistence.EntityManager;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
@@ -17,7 +22,8 @@ public class MessageService {
     private String uploadPath;
     private final MessageRepo messageRepo;
 
-    public MessageService(MessageRepo messageRepo) {
+
+    public MessageService(MessageRepo messageRepo, EntityManager em) {
         this.messageRepo = messageRepo;
     }
 
@@ -38,5 +44,16 @@ public class MessageService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Page<MessageDto> getPageMessages(Pageable pageable, String filter, User user) {
+        Page<MessageDto> pageMessages;
+        if (!filter.isBlank()) {
+            pageMessages = messageRepo.findByTag(filter, pageable, user);
+        } else {
+            pageMessages = messageRepo.findAll(pageable, user);
+            filter = null;
+        }
+        return pageMessages;
     }
 }
