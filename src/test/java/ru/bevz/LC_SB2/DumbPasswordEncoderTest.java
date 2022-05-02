7e96dcd0a230a82f6f4add1926c71cfb0db98629
Bevz Aleksandr <as-bivz@yandex.ru>
2022-05-02 22:56:03 +0300

    Add class DumbPasswordEncoderTest

---
+++ src/test/java/ru/bevz/LC_SB2/DumbPasswordEncoderTest.java
@@ -0,0 +1,18 @@
+package ru.bevz.LC_SB2;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DumbPasswordEncoderTest {
+
+    @Test
+    void encode() {
+        DumbPasswordEncoder encoder = new DumbPasswordEncoder();
+
+        assertEquals("secret: 'mypwd'", encoder.encode("mypwd"));
+        Assert.assertThat(encoder.encode("mypwd"), Matchers.containsString("mypwd"));
+    }
+}
